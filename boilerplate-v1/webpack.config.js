01efbc60ea9d420efc805205a680c1468937376e
Kelsi Proulx <kelsiproulx1919@gmail.com>
2021-02-23 12:46:43 -0800

    Set up boilerplate, expensify app, and react router

---
+++ boilerplate-v1/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+// This will give us the path to the current location on our machine
+// console.log(__dirname)
+// This will give us the actual path that we want (webpack can't read the path as it is)
+// console.log(path.join(__dirname, 'public'))
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      loader: 'babel-loader',
+      test: /\.js$/,
+      exclude: /node_modules/
+    }, {
+      test: /\.s?css$/,
+      use: [
+        'style-loader',
+        'css-loader',
+        'sass-loader'
+      ]
+    }]
+  },
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'public')
+  }
+};
