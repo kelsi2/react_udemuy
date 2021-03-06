9f468c09e4e196c240054623f2333c218009a57a
Kelsi Proulx <kelsiproulx1919@gmail.com>
2021-03-06 09:54:12 -0800

    Setup fetch expenses from firebase

--- expensify-app/src/tests/fixtures/expenses.js
+++ expensify-app/src/tests/fixtures/expenses.js
@@ -3,19 +3,19 @@ import moment from 'moment';
 export default [{
   id: '1',
   description: 'Gum',
-  node: '',
+  note: '',
   amount: 195,
   createdAt: 0
 }, {
   id: '2',
   description: 'Rent',
-  node: '',
+  note: '',
   amount: 109500,
   createdAt: moment(0).subtract(4, 'days').valueOf()
 }, {
   id: '3',
   description: 'Credit Card',
-  node: '',
+  note: '',
   amount: 4500,
   createdAt: moment(0).add(4, 'days').valueOf()
 }];
